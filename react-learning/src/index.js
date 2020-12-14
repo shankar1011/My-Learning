07e532b2ef734c639ce4479fa79ba51f7152b9fe
shankar1011 <shankarupadhyay1011@gmail.com>
2020-12-14 22:22:33 +0530

    added css used state and refactored the code

--- react-learning/src/index.js
+++ react-learning/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 /* const App = () => {
     window.navigator.geolocation.getCurrentPosition(
@@ -28,18 +29,13 @@ By the time we are getting the geolocation the component has been rendered and w
  */
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        // This is the only time we do direct assignment to this.state
-        this.state = {lat: null, errorMessage: ''};
+    
+    state = {lat: null, errorMessage: ''};
 
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({lat: position.coords.latitude})
-            },
-            err => {
-                this.setState({errorMessage: err.message})
-            }
+            position => this.setState({lat: position.coords.latitude}),
+            err => this.setState({errorMessage: err.message})
         );
     }
     render() {
@@ -48,7 +44,7 @@ class App extends React.Component {
         }
 
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat} />
         }
 
         return <div>Loading!</div>
