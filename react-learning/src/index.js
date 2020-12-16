d20051e1dcaefb9225d5cbf0903a29a0a8bd1f52
shankar1011 <shankarupadhyay1011@gmail.com>
2020-12-16 12:52:23 +0530

    added spinner and default props to the spinner

--- react-learning/src/index.js
+++ react-learning/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 /* const App = () => {
     window.navigator.geolocation.getCurrentPosition(
@@ -48,7 +49,7 @@ class App extends React.Component {
             return <SeasonDisplay lat={this.state.lat} />
         }
 
-        return <div>Loading!</div>
+        return <Spinner message="Please accept location request" />
     }
 
     render() {
