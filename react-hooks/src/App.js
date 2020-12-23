0df40a64d66a0bc05e58cd72a765cdb82359555e
shankar1011 <shankarupadhyay1011@gmail.com>
2020-12-23 21:16:28 +0530

    added search component using hooks

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import Accordion from './components/Accordion';
+import React from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
 const items = [
   {
-    title: 'What is React?',
-    content: 'React is a front end javascript library',
+    title: "What is React?",
+    content: "React is a front end javascript library",
   },
   {
-    title: 'Why use React?',
-    content: 'React is a favorite JS library among engineers because of virtual DOM',
+    title: "Why use React?",
+    content:
+      "React is a favorite JS library among engineers because of virtual DOM",
   },
   {
-    title: 'How do you use React?',
-    content: 'You use React by creating components',
+    title: "How do you use React?",
+    content: "You use React by creating components",
   },
 ];
 
 const App = () => {
   return (
     <div>
-      <Accordion items={items} />
+      <Search />
     </div>
   );
 };
